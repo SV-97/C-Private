3a41e036d0bf079d39c0d93a50b54ae3c354d3cb
SV-97 <volzstefan97@googlemail.com>
2017-10-26 20:44:52 +0200

    Add files via upload

---
+++ 001_ConsoleApp/001_ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is your name? "); //Print Line
+            string name = Console.ReadLine(); //assign variable "name" with data type "string" value from console input
+            Console.WriteLine("Hello " + name); //Print New Line with string and Variable
+
+            var variable1 = "Schoki";
+            Console.WriteLine(variable1);
+            //variable1 = 123; //not allowed since variable is defined with different data type
+            Console.WriteLine("variable1 is a {0}", variable1.GetTypeCode()); //Outputs data type of variable
+
+            Console.WriteLine("Enter integer value");
+            int number = Convert.ToInt16(Console.ReadLine()); //Read string from console, convert to Int16, assign to variable
+            Console.WriteLine("Is {0} number {1} bigger 9 OR is {0} number {1} smaller or equal to 7?");
+            if ((number > 9) || (number <= 7)) //If statement with logic OR (&& AND ; || OR ; ^ XOR ; ! NOT)
+            {
+                Console.WriteLine("Yes it is");
+            } else {
+                Console.WriteLine("No it isn't");
+            }
+
+            for (int i = 0; i < 10; i++) //For integer "i" from 1-9 in increments of 1
+            {
+                Console.Write(i); //Print "i" to console
+            }
+
+
+
+        }
+    }
+}
---
+++ 002_FunctionPlotter/002_FunctionPlotter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_FunctionPlotter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Function y=m*x+t"); //Print general syntax of function
+
+            int m = 1; //default value m
+            int t = 0; //default value t
+
+            /* How to isNullOrEmpty?
+            string mTemp = Console.ReadLine()
+            if ( mTemp.isNullOrEmpty )
+            { };
+            */
+
+            Console.WriteLine("Input m: ");  m = Convert.ToInt32(Console.ReadLine()); //read value for m
+            Console.WriteLine("Input t: ");  t = Convert.ToInt32(Console.ReadLine());//read value for t
+            Console.WriteLine("Start: "); int start = Convert.ToInt32(Console.ReadLine());//read start value for plotting
+            Console.WriteLine("End: "); int end = Convert.ToInt32(Console.ReadLine()); end = +1;//read end value for plotting
+            Console.WriteLine("Increment: "); int i = Convert.ToInt32(Console.ReadLine());//read increment size
+
+            Console.WriteLine("X        Y");
+
+            for (int x = start; x < end; x += i) //calculate values
+            {
+                int y = m * x + t;
+                Console.WriteLine( Convert.ToString(x));
+                Console.Write( Convert.ToString(y));
+            }
+        }
+    }
+}
