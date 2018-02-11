8f74421e790521fd05a71c5df762f5b9a23628ed
SV-97 <volzstefan97@googlemail.com>
2018-02-11 16:40:48 +0100

    Add files via upload

---
+++ 006_TicTacToe/006_TicTacToe/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace _006_TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label00 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(54, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(54, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(54, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "2";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(74, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(100, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "1";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(126, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "2";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(205, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.Text = "Player";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(205, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(35, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "AI";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label00
+            // 
+            this.label00.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label00.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label00.Location = new System.Drawing.Point(74, 72);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(20, 20);
+            this.label00.TabIndex = 17;
+            this.label00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label00.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label10.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label10.Location = new System.Drawing.Point(100, 72);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 20);
+            this.label10.TabIndex = 18;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label20
+            // 
+            this.label20.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label20.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label20.Location = new System.Drawing.Point(126, 72);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(20, 20);
+            this.label20.TabIndex = 19;
+            this.label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label20.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label01
+            // 
+            this.label01.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label01.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label01.Location = new System.Drawing.Point(74, 96);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(20, 20);
+            this.label01.TabIndex = 20;
+            this.label01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label01.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label02
+            // 
+            this.label02.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label02.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label02.Location = new System.Drawing.Point(74, 122);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(20, 20);
+            this.label02.TabIndex = 21;
+            this.label02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label02.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label11.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label11.Location = new System.Drawing.Point(100, 96);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(20, 20);
+            this.label11.TabIndex = 22;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label11.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label12.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label12.Location = new System.Drawing.Point(100, 122);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(20, 20);
+            this.label12.TabIndex = 23;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label12.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label21
+            // 
+            this.label21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label21.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label21.Location = new System.Drawing.Point(126, 96);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(20, 20);
+            this.label21.TabIndex = 24;
+            this.label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label21.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // label22
+            // 
+            this.label22.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label22.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label22.Location = new System.Drawing.Point(126, 122);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(20, 20);
+            this.label22.TabIndex = 25;
+            this.label22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label22.Click += new System.EventHandler(this.Field_Clicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 168);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label02);
+            this.Controls.Add(this.label01);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label00);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label00;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label01;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 006_TicTacToe/006_TicTacToe/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _006_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            areaLabels[0, 0] = label00;
+            areaLabels[1, 0] = label10;
+            areaLabels[2, 0] = label20;
+            areaLabels[0, 1] = label01;
+            areaLabels[1, 1] = label11;
+            areaLabels[2, 1] = label21;
+            areaLabels[0, 2] = label02;
+            areaLabels[1, 2] = label12;
+            areaLabels[2, 2] = label22;
+        }
+
+        public bool turn = false; //false = player 1
+        char[,] area = new char[3, 3]; //[x,y]
+        Label[,] areaLabels = new Label[3, 3];
+
+        private void Field_Clicked(object sender, EventArgs e)
+        {
+            Label current_label = sender as Label;
+            int current_x = Convert.ToInt32(current_label.Name.Remove(0, current_label.Name.Length-2).Remove(1,1)); //extract current x coordinate from label name
+            int current_y = Convert.ToInt32(current_label.Name.Remove(0, current_label.Name.Length - 1)); //extract current y coordinate from label name
+           
+            if (radioButton1.Checked) //selection player/AI
+            {
+                if (area[current_x, current_y] == '\0') //if clicked field is empty
+                {
+                    if (turn) //check which players turn it is and select corresponding char
+                    {
+                        area[current_x, current_y] = 'X';
+                        turn = false;
+                    }
+                    else
+                    {
+                        area[current_x, current_y] = 'O';
+                        turn = true;
+                    }
+                }
+                current_label.Text = Convert.ToString(area[current_x, current_y]);
+            }
+            else
+            {
+                if (area[current_x, current_y] == '\0') //if clicked field is empty
+                {
+                    area[current_x, current_y] = 'X';
+                    current_label.Text = Convert.ToString(area[current_x, current_y]);
+                    turn = false;
+                    CheckWin();
+                    AI();
+                }
+            }
+            CheckWin();
+        }
+
+        private void AI()
+        {
+            Random random = new Random();
+            int ai_x = random.Next(0, 3);
+            int ai_y = random.Next(0, 3);
+            int filledFields = 0;
+            foreach (var field in area)
+            {
+                if (field != '\0')
+                {
+                    filledFields++;
+                }
+            }
+            if (filledFields >= 9)
+            {
+                return;
+            }
+            while (area[ai_x, ai_y] != '\0')
+            {
+                ai_x = random.Next(0, 3);
+                ai_y = random.Next(0, 3);
+            }
+            area[ai_x,ai_y] = 'O';
+            areaLabels[ai_x, ai_y].Text = Convert.ToString(area[ai_x, ai_y]);
+            turn = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearArea();
+        }
+
+       private void ClearArea()
+        {
+            for (int i = 0; i < area.GetLength(0); i++)
+            {
+                area[i, 0] = '\0';
+                area[i, 1] = '\0';
+                area[i, 2] = '\0';
+                areaLabels[i, 0].Text = "";
+                areaLabels[i, 1].Text = "";
+                areaLabels[i, 2].Text = "";
+            }
+        }
+
+        private void CheckWin()
+        {
+            bool win = false;
+            char winning_sign = '\0';
+            string winning_party = "";
+            for (int i = 0; i < area.GetLength(0); i++)
+            {
+                if (area[i, 0] != '\0')
+                {
+                    if (area[i, 0] == area[i, 1]) //check for horizontal wincase
+                    {
+                        if (area[i, 0] == area[i, 2])
+                        {
+                            win = true;
+                            winning_sign = area[i, 0];
+                        }
+                    }
+
+                    if (area[0, i] == area[1, i]) //check for vertical wincase
+                    {
+                        if (area[0, i] == area[2, i])
+                        {
+                            win = true;
+                            winning_sign = area[0, i];
+                        }
+                    }
+                }
+            }
+
+            if (area[1,1] != '\0')
+            {
+                if (area[0, 0] == area[1, 1]) //check diagonal wincase 1
+                {
+                    if (area[0, 0] == area[2, 2])
+                    {
+                        win = true;
+                        winning_sign = area[1, 1];
+                    }
+                }
+
+                if (area[2, 0] == area[1, 1]) //check diagonal wincase 2
+                {
+                    if (area[2, 0] == area[0, 2])
+                    {
+                        win = true;
+                        winning_sign = area[1, 1];
+                    }
+                }
+            }
+            
+            if (win)
+            {
+                if (winning_sign == 'X')
+                {
+                    winning_party = "Player 1";
+                }
+                else
+                {
+                    if (radioButton1.Checked)
+                    {
+                        winning_party = "Player 2";
+                    }
+                    else
+                    {
+                        winning_party = "AI";
+                    }
+                }
+                MessageBox.Show(String.Format("Game Over! {0} won", winning_party),"Game Over!");
+                ClearArea();
+            }
+        }
+    }
+}
